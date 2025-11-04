a4256546b366bad47b2a1b5debb770c7729d89e1
Ojas Parikh <ojas@Ojass-MacBook-Pro.local>
2025-11-04 23:05:01 +0000

    f1_fsm

---
+++ autumn/workspace/labs/Lab3-FSM-main/task2/f1_fsm_tb.cpp
@@ -0,0 +1,56 @@
+#include "Vf1_fsm.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "vbuddy.cpp"
+
+int main(int argc, char **argv, char **env)
+{
+
+    int i;
+    int clk;
+
+    Verilated::commandArgs(argc, argv);
+    // init top verilog instance
+    Vf1_fsm *top = new Vf1_fsm;
+    // init trace dump
+    Verilated::traceEverOn(true);
+    VerilatedVcdC *tfp = new VerilatedVcdC;
+    top->trace(tfp, 99);
+    tfp->open("f1_fsm.vcd");
+
+    // init Vbuddy
+    if (vbdOpen() != 1)
+        return (-1);
+    vbdHeader("F1_FSM");
+
+    // initialize simulation inputs
+    top->clk = 1;
+    top->rst = 0;
+    top->en = 0;
+
+    // run simulation for many clock cycles
+    for (i = 0; i < 1000; i++)
+    {
+
+        top->en = vbdFlag();
+
+        // dump variables into VCD file and toggle clock
+        for (clk = 0; clk < 2; clk++)
+        {
+            tfp->dump(2 * i + clk); // unit is in ps!!!
+            top->clk = !top->clk;
+            top->eval();
+        }
+        vbdBar(top->data_out & 0xFF);
+        top->en = 0;
+
+        // either simulation finished, or 'q' is pressed
+        if ((Verilated::gotFinish()) || (vbdGetkey() == 'q'))
+            exit(0); // ... exit if finish OR 'q' pressed
+    }
+
+    vbdClose();
+
+    tfp->close();
+    exit(0);
+}
